bf337e0f7fd68fe32069494e91a97b6a5a9855de
Arekyus <alextsilva86@gmail.com>
2021-08-15 11:12:05 -0300

    Inicio de exercicio

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -12,14 +12,29 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+            
             Funcionario carlos = new Funcionario();
 
             carlos.Nome = "Carlos";
             carlos.CPF = "546879157-20";
             carlos.Salario = 2000;
 
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "454.658.148-3";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
 
             Console.ReadLine();
         }
